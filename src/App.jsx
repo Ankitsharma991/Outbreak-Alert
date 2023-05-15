65247464bf5ec24f748c3b8b5d93f66f3b106037
Ankit Sharma <sharmaa7861@gmail.com>
2023-05-15 18:43:39 +0530

    intial project cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-
   return (
     <div>
-      Covid-19 Tracker
+      <h1>Let's build a Covid-19 Tracker</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
